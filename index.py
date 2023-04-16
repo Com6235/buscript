089ecf2b6d8283f0be975d872f3fc1245a562663
Com6235 <130489282+Com6235@users.noreply.github.com>
2023-04-16 21:52:05 +0300

    abc

--- index.py
+++ index.py
@@ -1,2 +1,34 @@
 import pygame
-print("Hello, World!")
+
+# Initialize Pygame
+pygame.init()
+
+# Set the screen dimensions
+screen_width = 800
+screen_height = 600
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# Set the window title
+pygame.display.set_caption("My Python Game Screen")
+
+# Set the background color
+background_color = (255, 255, 255)
+
+# Start the main game loop
+running = True
+while running:
+    # Clear the screen with the background color
+    screen.fill(background_color)
+
+    # Process events (keyboard, mouse, etc.)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Update the screen
+    pygame.display.flip()
+    clock.tick(60)
+
+# Quit Pygame
+pygame.quit()
